c51cde6cabf62501c3229d7c825e7d4069fc9d6e
Mateo Morales García <mateomogar@gmail.com>
2021-02-24 15:02:13 -0500

    Add player movement animation

--- Assets/Scripts/ControlMovement.cs
+++ Assets/Scripts/ControlMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ControlMovement : MonoBehaviour
 {
     [SerializeField] float speed = 4;
+    [SerializeField] Animator animator;
 
     void Update()
     {
@@ -13,7 +14,15 @@ public class ControlMovement : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        ModifyAnimation(h, v);
+
         transform.position += new Vector3(h, v, 0) * (Time.deltaTime * speed);
+    }
+
+    void ModifyAnimation(float h, float v)
+    {
+        animator.SetFloat("horizontal", h);
+        animator.SetFloat("vertical", v);
     }
 
 }
