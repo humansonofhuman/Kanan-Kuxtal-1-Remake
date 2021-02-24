7430172175f806976aaa6ff7fb8da4d4d9a30288
Mateo Morales García <mateomogar@gmail.com>
2021-02-24 13:23:29 -0500

    Add damageable behaviour

---
+++ Assets/Scripts/Damageable.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Damageable : MonoBehaviour
+{
+    public event UnityAction OnDie;
+    public bool IsDead;
+    float maxDurability;
+    float durability;
+    public void TakeDamage(float amount)
+    {
+        durability -= amount;
+        if (durability <= 0)
+        {
+            IsDead = true;
+            OnDie?.Invoke();
+        }
+    }
+
+    public void RecoverDurability(float amount)
+    {
+        durability += amount;
+        if(durability > maxDurability)
+            durability = maxDurability;
+    }
+
+}
