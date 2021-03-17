e9bdf7bb726eeeae77c4e2aa08318ddb3d3eb725
humansonofhuman <mateomogar@gmail.com>
2021-03-17 12:16:03 -0500

    Add conditions to stop chasing
    Using On collision stay and enter

--- Assets/Scripts/ChaseTarget.cs
+++ Assets/Scripts/ChaseTarget.cs
@@ -4,10 +4,12 @@ public class ChaseTarget : MonoBehaviour
 {
     public Transform Target;
     [SerializeField] float speed = 3f;
+    bool isMoving = true;
 
     void Update()
     {
-        Chase();
+        if (isMoving)
+            Chase();
     }
 
     void Chase()
@@ -15,6 +17,27 @@ public class ChaseTarget : MonoBehaviour
         Vector2 direction = Target.position - transform.position;
         direction.Normalize();
         transform.position += (Vector3)direction * (Time.deltaTime * speed);
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (IsTarget(other))
+        {
+            isMoving = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (IsTarget(other))
+        {
+            isMoving = true;
+        }
+    }
+
+    private bool IsTarget(Collision2D other)
+    {
+        return other.gameObject.Equals(Target.gameObject);
     }
 
 }
